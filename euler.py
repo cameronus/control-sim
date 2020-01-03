c4acd5525e48f5d39907abb45010cdf1bdccb33a
Cameron Jones <14024224+cameronus@users.noreply.github.com>
2020-01-03 01:09:13 -1000

    Solved non-rotated linear force issue

--- euler.py
+++ euler.py
@@ -6,6 +6,7 @@ from vpython import *
 
 mass = 1.
 inertia_mat = np.matrix([[mass / 6, 0, 0], [0, mass / 6, 0], [0, 0, mass / 6]])
+thrust_origin = np.array([0, 0, -0.5])
 
 position = np.zeros((3))
 velocity = np.zeros((3))
@@ -24,15 +25,13 @@ cs = lambda: round(t / dt)
 
 position_hist = np.zeros((num_steps, 3))
 orientation_hist = np.empty((num_steps), dtype=np.quaternion)
-force_hist = np.zeros((num_steps, 3))
+thrust_hist = np.zeros((num_steps, 3))
 
 def apply_forces():
-    gravity = np.array([0., 0., -9.81])
-    # push = np.array([0.2, 0.1, 0.])
-    push = np.array([np.cos(20 * t), np.sin(20 * t), 9.81]) # issue: push force is applied linearly no matter the orientation
-    force_hist[cs()] = push
-    force = gravity + push
-    torque = np.cross(push, np.array([0, 0, -0.5]))
+    thrust = np.array([-np.cos(10 * t), 0., 10.]) # issue: push force is applied linearly no matter the orientation
+    thrust_hist[cs()] = thrust
+    force = np.array([0, 0, -9.81]) + quaternion.rotate_vectors(orientation, thrust) # solution: rotate linear force vector with body
+    torque = np.cross(thrust, thrust_origin)
     return force, torque
 
 while t <= t_end:
@@ -60,31 +59,24 @@ o = o[::30]
 ax.quiver(p[:,0], p[:,1], p[:,2], p[:,0] + o[:,0], p[:,1] + o[:,1], p[:,2] + o[:,2], length = 100, normalize = True)
 plt.show()
 """
-scene = canvas(background=color.white, width=1920, height=1080)
+scene = canvas(background=color.white, width=1760, height=900) # title='Some title', caption='A caption'
 scene.up = vector(0, 0, 1)
-scene.forward = vector(1,0,0)
+scene.forward = vector(-1,0,0)
 xaxis = cylinder(pos=vec(0,0,0), axis=vec(20,0,0), radius=0.2, color=color.red)
 yaxis = cylinder(pos=vec(0,0,0), axis=vec(0,20,0), radius=0.2, color=color.green)
 zaxis = cylinder(pos=vec(0,0,0), axis=vec(0,0,20), radius=0.2, color=color.blue)
-k = 1.02
-h = 1
-text(pos=xaxis.pos + k * xaxis.axis, text='x', height=h, align='center', billboard=True, color=color.black)
-text(pos=yaxis.pos + k * yaxis.axis, text='y', height=h, align='center', billboard=True, color=color.black)
-text(pos=zaxis.pos + k * zaxis.axis, text='z', height=h, align='center', billboard=True, color=color.black)
-b = box(pos = vector(0, 0, 0), size=vector(1, 1, 1), color=color.blue, make_trail=True)
+text(pos=xaxis.pos + 1.02 * xaxis.axis, text='x', height=1, align='center', billboard=True, color=color.black)
+text(pos=yaxis.pos + 1.02 * yaxis.axis, text='y', height=1, align='center', billboard=True, color=color.black)
+text(pos=zaxis.pos + 1.02 * zaxis.axis, text='z', height=1, align='center', billboard=True, color=color.black)
+b = box(pos = vector(0, 0, 0), size=vector(1, 1, 1), color=color.blue, make_trail=True) # trail_type='points', interval=10
 scene.camera.follow(b)
 v = arrow(pos=vector(0, 0, 0), color=color.yellow)
-for pos, rot, force in zip(position_hist, orientation_hist, force_hist):
-    # print(rot)
+for pos, rot, thrust_force in zip(position_hist, orientation_hist, thrust_hist):
     up = quaternion.rotate_vectors(rot, np.array([0, 0, 1]))
-
     b.pos = vector(*pos)
     b.up = vector(*up)
-
-    thrust_dir = quaternion.rotate_vectors(rot, np.array([0, 0, -0.5]))
-    thrust = quaternion.rotate_vectors(rot, force)
-
-    v.pos = vector(*(pos + thrust_dir))
+    v.pos = vector(*(pos + quaternion.rotate_vectors(rot, thrust_origin)))
     # v.up = vector(*up)
-    v.axis = vector(*-thrust)
+    rotated_thrust = quaternion.rotate_vectors(rot, thrust_force)
+    v.axis = vector(*-rotated_thrust / np.linalg.norm(rotated_thrust))
     sleep(0.0001)

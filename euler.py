aa52cfa83b4d4854dbe0a03d39902d79787c1894
Cameron Jones <14024224+cameronus@users.noreply.github.com>
2020-01-03 21:19:24 -1000

    Implemented state vector

--- euler.py
+++ euler.py
@@ -20,27 +20,21 @@ t = 0
 t_end = 10
 dt = 1/100
 
-num_steps = round(t_end / dt) + 1
-cs = lambda: round(t / dt)
-
-position_hist = np.zeros((num_steps, 3))
-orientation_hist = np.empty((num_steps), dtype=np.quaternion)
-thrust_hist = np.zeros((num_steps, 3))
+states = np.zeros((round(t_end / dt) + 1, 16)) # [x, y, z, vx, vy, vz, ow, ox, oy, oz, ωx, ωy, ωz, tx, ty, tz]
 
 def control_alg(): # reducing accuracy of provided data (adding noise to simulate IMU noise)
-    return np.array([-5 * np.cos(5 * t), 0., 10.])
+    return np.array([np.sqrt(1 * 0.5**(t/10)), 0., 10.])
 
 def apply_forces(): # additional forces: drag (shear stress, friction torque), wind
     thrust = control_alg()
-    rot_thrust_force = thrust_hist[cs()] = quaternion.rotate_vectors(orientation, thrust) # BUG: inverted rotation
+    rot_thrust_force = quaternion.rotate_vectors(orientation, thrust)
     force = np.array([0, 0, -9.81]) + rot_thrust_force
-    torque = np.cross(thrust_origin, thrust) # solution: fixed flipped order for cross product
-    return force, torque
+    torque = np.cross(thrust_origin, thrust)
+    return force, torque, rot_thrust_force
 
 while t <= t_end: # implement ground collision for take-off/landing simulations
-    position_hist[cs()] = position
-    orientation_hist[cs()] = orientation
-    force, torque = apply_forces()
+    force, torque, rot_thrust = apply_forces()
+    states[round(t / dt)] = np.array([*position, *velocity, *quaternion.as_float_array(orientation), *omega, *rot_thrust])
     velocity += force / mass * dt
     position += velocity * dt
     orientation += orientation * np.quaternion(0, *omega) * 0.5 * dt
@@ -62,27 +56,26 @@ o = o[::30]
 ax.quiver(p[:,0], p[:,1], p[:,2], p[:,0] + o[:,0], p[:,1] + o[:,1], p[:,2] + o[:,2], length = 100, normalize = True)
 plt.show()
 """
+
 scene = canvas(background=color.white, width=1760, height=900) # title='Some title', caption='A caption'
 scene.up = vector(0, 0, 1)
-scene.forward = vector(-1,0,0)
-xaxis = cylinder(pos=vec(0,0,0), axis=vec(20,0,0), radius=0.2, color=color.red)
-yaxis = cylinder(pos=vec(0,0,0), axis=vec(0,20,0), radius=0.2, color=color.green)
-zaxis = cylinder(pos=vec(0,0,0), axis=vec(0,0,20), radius=0.2, color=color.blue)
+scene.forward = vector(-1, -1, -1)
+xaxis = cylinder(pos=vec(0, 0, 0), axis=vec(20, 0, 0), radius=0.2, color=color.red)
+yaxis = cylinder(pos=vec(0, 0, 0), axis=vec(0, 20, 0), radius=0.2, color=color.green)
+zaxis = cylinder(pos=vec(0, 0, 0), axis=vec(0, 0, 20), radius=0.2, color=color.blue)
 text(pos=xaxis.pos + 1.02 * xaxis.axis, text='x', height=1, align='center', billboard=True, color=color.black)
 text(pos=yaxis.pos + 1.02 * yaxis.axis, text='y', height=1, align='center', billboard=True, color=color.black)
 text(pos=zaxis.pos + 1.02 * zaxis.axis, text='z', height=1, align='center', billboard=True, color=color.black)
 b = box(pos = vector(0, 0, 0), size=vector(1, 1, 1), color=color.blue, make_trail=True) # trail_type='points', interval=10
 scene.camera.follow(b)
 v = arrow(pos=vector(0, 0, 0), color=color.yellow)
-vv = arrow(pos=vector(0, 0, 0), color=color.purple)
-for pos, rot, thrust_force in zip(position_hist, orientation_hist, thrust_hist):
-    up = quaternion.rotate_vectors(rot, np.array([0, 0, 1]))
-    b.pos = vector(*pos)
+for state in states:
+    position, velocity, orientation, omega, thrust = np.split(state, [3, 6, 10, 13]) # position, velocity, orientation, angular velocity, thrust
+    orientation = quaternion.from_float_array(orientation)
+    up = quaternion.rotate_vectors(orientation, np.array([0, 0, 1]))
+    b.pos = vector(*position)
     b.up = vector(*up)
-    v.pos = vector(*(pos + quaternion.rotate_vectors(rot, thrust_origin)))
+    v.pos = vector(*(position + quaternion.rotate_vectors(orientation, thrust_origin)))
     # v.up = vector(*up)
-    v.axis = vector(*-thrust_force / np.linalg.norm(thrust_force) * 3)
-
-    vv.pos = vector(*(pos + quaternion.rotate_vectors(rot, thrust_origin)))
-    vv.axis = vector(*thrust_force / np.linalg.norm(thrust_force) * 3)
+    v.axis = vector(*-thrust / np.linalg.norm(thrust) * 2)
     sleep(0.0001)

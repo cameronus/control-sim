971827dfe4c036a905317f9839b42da677d258d2
Cameron Jones <14024224+cameronus@users.noreply.github.com>
2020-01-04 17:51:33 -1000

    Basic PID control

--- euler.py
+++ euler.py
@@ -3,8 +3,10 @@ import quaternion
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from vpython import *
+from simple_pid import PID
 
 mass = 1.
+edf_force = 14.4207
 inertia_mat = np.matrix([[mass / 6, 0, 0], [0, mass / 6, 0], [0, 0, mass / 6]])
 thrust_origin = np.array([0, 0, -0.5])
 
@@ -12,7 +14,8 @@ position = np.zeros((3))
 velocity = np.zeros((3))
 # velocity = np.array([10., 5., 30.])
 
-orientation = np.quaternion(1, 0, 0, 0)
+# orientation = np.quaternion(1, 0, 0, 0)
+orientation = np.quaternion(1, 0.5, 0.2, 0)
 omega = np.zeros((3))
 # omega = np.array([1., 0., -0.2])
 
@@ -22,23 +25,37 @@ t, t_end = 0, 10
 dt = 1 / 1000
 refresh = 500 # Hz
 
-states = np.zeros((round(t_end / dt) + 1, 16)) # [x, y, z, vx, vy, vz, ow, ox, oy, oz, ωx, ωy, ωz, tx, ty, tz]
+states = np.zeros((round(t_end / dt) + 1, 19)) # [x, y, z, vx, vy, vz, ow, ox, oy, oz, ωx, ωy, ωz, tx, ty, tz]
 
-
-thrust = np.array([0, 0, 0])
+tuning = 2.5, 0.1, 0.5
+pid_x = PID(*tuning, setpoint=0)
+pid_y = PID(*tuning, setpoint=0)
+thrust = np.array([0, 0, edf_force])
 
 def control_alg(acceleration): # reducing accuracy of provided data (adding noise to simulate IMU noise)
     # available: orientation, angular velocity, acceleration
-    mu, sigma = 0, 0.1
-    noise = np.random.normal(mu, sigma, (3))
-    print(round(t, 4))
-    print(orientation)
-    print(omega)
-    print(acceleration)
-    print()
-    return np.array([0., -0.01, 12.])
+    # mu, sigma = 0, 0.1
+    # noise = np.random.normal(mu, sigma, (3))
+    # print(round(t, 4))
+    # print(quaternion.as_float_array(orientation)[1])
+    # print(quaternion.as_euler_angles(orientation))
+    # print(omega)
+    # print(acceleration)
+    # print()
 
-def apply_forces(): # external forces: gravity, drag (shear stress, friction torque), wind
+    # compute new ouput from the PID according to the sy
+    rot = quaternion.as_rotation_vector(orientation)
+    # rot_mag = np.sqrt(rot[0]**2 + rot[1]**2 + rot[2]**2)
+    # control_x = pid_x(rot_mag)
+    # control_y = pid_y(rot_mag)
+    control_x = pid_x(quaternion.as_float_array(orientation)[1])
+    control_y = pid_y(quaternion.as_float_array(orientation)[2])
+    # feed the PID output to the system and get its curr
+    # v = controlled_system.update(control)
+
+    return np.array([control_x, control_y, edf_force])
+
+def apply_forces(): # external forces: gravity, drag (shear stress, friction torque), wind; TODO: implement torque component of drag
     wind = np.array([1, 2.5, 0])
     drag = 0.5 * 1.225 * 1 * 1 * (velocity - wind) * np.absolute(velocity - wind) # Fd = 1/2 * ρ * Cd * A * v^2
     force = np.array([0, 0, -9.81]) - drag
@@ -51,9 +68,10 @@ while t <= t_end: # implement ground collision for take-off/landing simulations
     rot_thrust = quaternion.rotate_vectors(orientation, thrust)
 
     force = ext_force + rot_thrust
+    print(t, rot_thrust)
     torque = np.cross(thrust_origin, thrust)
 
-    states[round(t / dt)] = np.array([*position, *velocity, *quaternion.as_float_array(orientation), *omega, *rot_thrust])
+    states[round(t / dt)] = np.array([*position, *velocity, *quaternion.as_float_array(orientation), *omega, *thrust, *rot_thrust])
 
     velocity += force / mass * dt
     position += velocity * dt
@@ -83,16 +101,18 @@ v = arrow(pos=vector(0, 0, 0), color=color.yellow)
 scale_factor = 10
 states = states[::scale_factor]
 
+sleep(1)
+
 for i, state in enumerate(states):
-    position, velocity, orientation, omega, thrust = np.split(state, [3, 6, 10, 13]) # position, velocity, orientation, angular velocity, thrust
-
-    scene.title = f't={round(i * scale_factor * dt, 2)}s<br>position: {np.array_str(position, precision=3)}<br>velocity: {np.array_str(velocity, precision=3)}'
-
+    position, velocity, orientation, omega, thrust, rot_thrust = np.split(state, [3, 6, 10, 13, 16]) # position, velocity, orientation, angular velocity, thrust
     orientation = quaternion.from_float_array(orientation)
+
+    scene.title = f't={round(i * scale_factor * dt, 2)}s<br>position: {np.array_str(position, precision=3)}<br>velocity: {np.array_str(velocity, precision=3)}<br>thrust: {np.array_str(thrust, precision=3)}'
+
     up = quaternion.rotate_vectors(orientation, np.array([0, 0, 1]))
     b.pos = vector(*position)
     b.up = vector(*up)
     v.pos = vector(*(position + quaternion.rotate_vectors(orientation, thrust_origin)))
-    v.axis = vector(*-thrust / np.linalg.norm(thrust) * 2)
-    # v.up = vector(*up)
+    # v.up = vector(*up) # TODO: set vector to one perpendicular to the axis in the direction closest to the box
+    v.axis = vector(*-rot_thrust / np.linalg.norm(rot_thrust) * 1.5)
     sleep(0.000001)
